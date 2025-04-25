4c06e65cc74aa23d9916512033a48392ab745453
HwangHyeonseok <hhs0995@naver.com>
2025-04-25 19:37:02 +0900

    lecture:4-9_스프링으로 전환하기 (스프링 빈)

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,22 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appconfig = new AppConfig();
-        MemberService memberService = appconfig.memberService();
-//        MemberService memberService = new MemberServiceImpl();
+        // DIP, OCP 위반
+        //        MemberService memberService = new MemberServiceImpl();
+
+        // 순수 JAVA AppConfig 사용
+//        AppConfig appconfig = new AppConfig();
+//        MemberService memberService = appconfig.memberService();
+
+        // Spring Bean 사용
+        // applicationContext : 스프링 컨테이너
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class); // AppConfig 클래스에서 Bean으로 등록한 것을 applicationContext 컨테이너에 넣는다.
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);// memberService 객체를 찾는다. 그 타입은 MemberService 타입이다.
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -7,15 +7,24 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appconfig = new AppConfig();
-        MemberService memberService = appconfig.memberService();
-        OrderService orderService = appconfig.orderService();
-
+        // OCP, DIP 위반
 //        MemberService memberService = new MemberServiceImpl();
 //        OrderService orderService = new OrderServiceImpl();
+
+        // 순수 JAVA AppConfig
+//        AppConfig appconfig = new AppConfig();
+//        MemberService memberService = appconfig.memberService();
+//        OrderService orderService = appconfig.orderService();
+
+        // Spring Bean 이용
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class); //  AppConfig 클래스 안에 등록된 Bean들을 스프링 컨테이너인 applicationContext에 넣는다.
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); //  applicationContext 컨테이너에서 빈 이름이 memberService인 것을 가져온다. 근데 그 객체의 리턴형은 MemberService 이다.
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class); //  //  applicationContext 컨테이너에서 빈 이름이 orderService인 것을 가져온다. 근데 그 객체의 리턴형은 OrderService 이다.
 
         long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
