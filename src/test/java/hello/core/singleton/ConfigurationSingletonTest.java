b56d9286206d696fd1df4e0bd6b259531fb9b777
HwangHyeonseok <hhs0995@naver.com>
2025-04-27 14:53:55 +0900

    lecture:6-5_@Configuration과 싱글톤

---
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,43 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.Order.OrderServiceImpl;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+    // AppConfig.java에서
+    // memberService() -> new MemoryMemberRepository() 호출
+    // orderService() -> new MemoryMemberRepository() 호출
+
+    // new가 2번이나 되는데, 각 주소가 그러면 다 다른거 아닌가?
+    // 그러면 스프링 컨테이너에서 관리하는 빈들도 참조(주소)가 다 다를 수 있는 것 아닌가? -> 싱글톤이 깨지는것 아닌가?
+
+
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberService -> memberRepository = " + memberRepository1);
+        System.out.println("orderService -> memberRepository = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+
+    }
+}
